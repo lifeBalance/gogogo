9e453e02c6b35c8d4e18426760c18ac10f187f10
Javi Rodriguez <lifeBalance@users.noreply.github.com>
2026-09-24 19:38:51 +0300

    Work on running-sum

--- solutions/running-sum/running_sum.go
+++ solutions/running-sum/running_sum.go
@@ -4,7 +4,13 @@ import "fmt"
 
 // RunningSum returns a slice where element i is the sum of nums[0] through nums[i].
 func RunningSum(nums []int) []int {
-	panic("not implemented")
+	result := make([]int, len(nums))
+	sum := 0
+	for i, n := range nums {
+		sum += n
+		result[i] = sum
+	}
+	return result
 }
 
 func main() {
