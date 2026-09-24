25001be07f9da1928a73871826f6aad1c5b91212
Javi Rodriguez <lifeBalance@users.noreply.github.com>
2026-09-24 19:14:39 +0300

    Work on running-sum

---
+++ solutions/running-sum/running_sum.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// RunningSum returns a slice where element i is the sum of nums[0] through nums[i].
+func RunningSum(nums []int) []int {
+	panic("not implemented")
+}
+
+func main() {
+	fmt.Println(RunningSum([]int{1, 2, 3, 4}))
+}
